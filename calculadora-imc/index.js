4ea8443ee711c2417f97c5ca2db189fb324e7c0b
Mathx8 <matheusqueirozdev@gmail.com>
2025-08-06 14:27:52 -0300

    Atividade 2

--- calculadora-imc/index.js
+++ calculadora-imc/index.js
@@ -1,26 +1,37 @@
-function CalcularIMC() {
+function CalcularIMC(event) {
     event.preventDefault();
-    const peso = document.getElementById("peso").value;
-    const altura = document.getElementById("altura").value;
-    pesoReal = peso/100;
-    alturaReal = altura*100;
+    const peso = parseFloat(document.getElementById("peso").value);
+    const altura = parseFloat(document.getElementById("altura").value);
+    const span = document.getElementById("resultado");
 
-    const imc = pesoReal / (alturaReal * alturaReal);
+    if (!peso) {
+        document.querySelector("span").innerHTML = "<p>Digite seu peso!</p>";
+        span.style.display = "flex";
+        span.style.color = "red";
+        return;
+    }
+    if (!altura || altura <= 0) {
+        document.querySelector("span").innerHTML = "<p>Digite a altura corretamente!</p>";
+        span.style.display = "flex";
+        span.style.color = "red";
+        return;
+    }
+    const imc = peso / (altura * altura);
 
-    let resultado = '';
     let rIMC = '';
 
     if (imc <= 18.5) {
-        rIMC = "Abaixo do peso"
-    } else if (imc <= 18.6 && imc >= 24.9) {
-        return rIMC = "Peso Normal"
-    } else if (imc <= 25 && imc >= 29.9) {
-        return rIMC = "Sobrepeso"
+        rIMC = "Abaixo do peso";
+    } else if (imc <= 24.9) {
+        rIMC = "Peso Normal";
+    } else if (imc <= 29.9) {
+        rIMC = "Sobrepeso";
     } else {
-        return rIMC = "Obesidade"
+        rIMC = "Obesidade";
     }
 
-    resultado += `<h1>${imc}. Isso significa que você está ${rIMC}</h1>`;
-
+    const resultado = `${imc.toFixed(2)} - ${rIMC}`;
+    span.style.display = "flex";
+    span.style.fontSize = "1.2rem";
     document.querySelector("span").innerHTML = resultado;
-};
+}
